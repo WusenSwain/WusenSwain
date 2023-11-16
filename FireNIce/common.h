fb225165736c6cce140d1edda3bab2c343f609d2
WusenSwain <1208532105@qq.com>
2023-11-16 18:05:28 +0800

    森林冰火人代码

---
+++ FireNIce/common.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<stdio.h>
+#include<graphics.h>
+#include<time.h>
+#include<conio.h>
+#include<string>
+#include<vector>
+#include<iostream>
+#include<functional>
+#include"EasyxPng.h"
+using namespace std;
+
